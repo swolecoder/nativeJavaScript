ed7a6c44d78762d79bdef3a3e5e57c7f87d07d10
swolecoder <swolecoder@gmail.com>
2019-04-20 19:07:13 -0400

    reduce function

---
+++ reduce.js
@@ -0,0 +1,32 @@
+'use strict';
+var _ = require('lodash');
+
+/*
+Inpiration: 
+_.reduce(
+  [1, 2, 3],
+  function(sum, n) {
+    return sum + n;
+  },
+  0
+);
+*/
+
+const reduceV1 = (list, cb, intial) => {
+  let memo = intial;
+  for (let i = 0; i < list.length; i++) {
+    if (i === 0 && memo === undefined) {
+      memo = list[0];
+    } else {
+      memo = cb(list[i], memo);
+    }
+  }
+
+  return memo;
+};
+
+function sumV1(n, sum) {
+  return n + sum;
+}
+
+console.log(reduceV1([1, 2], sum));
